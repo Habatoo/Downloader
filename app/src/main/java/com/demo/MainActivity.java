753fa2a1f13ec0cbe62dea05232bd64dbecead78
Habatoo <habatoo@gmail.com>
2020-09-18 19:09:23 +0700

    final commit add download by click

--- app/src/main/java/com/demo/MainActivity.java
+++ app/src/main/java/com/demo/MainActivity.java
@@ -2,13 +2,83 @@ package com.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mailRu = "https://mail.ru";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        DownloadTask task = new DownloadTask();
+        try {
+            String result = task.execute(mailRu).get();
+            Log.i("URL", result);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickDownload(View view) {
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute(mailRu).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        textView.setText(result);
+    }
+
+    private static class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder result = new StringBuilder();
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection =  (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                BufferedReader buffereadReader = new BufferedReader(reader);
+                String line = buffereadReader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = buffereadReader.readLine();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return result.toString();
+        }
     }
 }
